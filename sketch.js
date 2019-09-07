72df6b28fc9c6660976b35a2918ae04376632468
Dhruv Singh <ds3638@columbia.edu>
2019-09-06 20:47:17 -0400

    final clock

--- sketch.js
+++ sketch.js
@@ -1,73 +1,109 @@
 let x = 0;
 function setup() {
 	createCanvas(700,700); // make an HTML canvas element width x height pixels
-	theta = 180;
 }
 
 function draw() {
-	background(225);
-	textSize(32);
-	translate(width/2, height/2);/*
-	fill(180);
-	text(hour(), -20, -20);
-	fill(100);
-	text(minute(), -20, 10);
-	fill(0);
-	text(second(), -20, 40);*/
-	fill(0);
-	
-	let center_x = 0, center_y = 0;
-	let radius = 75;
+	if (hour() > 18) {
+		background(0, 0, 80);
+	} else {
+		background(230, 247, 255);
+	}
+	translate(width/2, height/2);
+
+	let orbit_radius = 75;
 
 	x += 0.01;
 
-	if (second() % 10 > 0 && second % 10 < 6) {
-		let p1_x = center_x + radius * Math.sin(0.5*x), p1_y = center_y + radius * Math.cos(0.5*x);
-		let p2_x = center_x - radius * Math.sin(0.5*x), p2_y = center_y - radius * Math.cos(0.5*x);
-		ellipse(p1_x, p1_y, 50, 50);
-		ellipse(p2_x, p2_y, 50, 50);
+	// Disc radius in pixels for H:M:S = 50:40:20
+
+	// The discs for seconds
+	// Appear every second
+	if (second() % 2 == 0) {
+		let second_disc1_x = 0.2 * orbit_radius, second_disc1_y = 0;
+		let second_disc2_x = -0.2 * orbit_radius, second_disc2_y = 0;
+		if (hour() > 12) {
+			fill(225);
+		} else {
+			fill(20);
+		}
+		
+		noStroke();
+		ellipse(second_disc1_x, second_disc1_y, 20, 20);
+		ellipse(second_disc2_x, second_disc2_y, 20, 20);
 	}
-	let p3_x = center_x - radius * Math.sin(x), p3_y = center_y + radius * Math.cos(x);
-	let p4_x = center_x + radius * Math.sin(x), p4_y = center_y - radius * Math.cos(x);
 
-	ellipse(p3_x, p3_y, 50, 50);
-	ellipse(p4_x, p4_y, 50, 50);
+	// The discs for the minutes
+	fill(90);
+	orbit_radius = orbit_radius * 2;
 
-	let p5_x = center_x - radius * Math.sin(-x), p5_y = center_y + radius * Math.cos(-x);
-	let p6_x = center_x + radius * Math.sin(-x), p6_y = center_y - radius * Math.cos(-x);
+	let minute_disc3_x = - orbit_radius * Math.sin(-0.75*x), minute_disc3_y = + orbit_radius * Math.cos(-0.75*x);
+	let minute_disc4_x = + orbit_radius * Math.sin(-0.75*x), minute_disc4_y = - orbit_radius * Math.cos(-0.75*x);
+	let minute_disc7_x = - orbit_radius * Math.sin(0.75*x), minute_disc7_y = + orbit_radius * Math.cos(0.75*x);
+	let minute_disc8_x = + orbit_radius * Math.sin(0.75*x), minute_disc8_y = - orbit_radius * Math.cos(0.75*x);
 
-	ellipse(p5_x, p5_y, 50, 50);
-	ellipse(p6_x, p6_y, 50, 50);
+	if (minute() > 15) {
+		ellipse(minute_disc3_x, minute_disc3_y, 40, 40);
+	}
+	if (minute() > 30) {
+		ellipse(minute_disc4_x, minute_disc4_y, 40, 40);
+	}
+	orbit_radius = orbit_radius * 1.5;
+	if (minute() > 45) {
+		ellipse(minute_disc7_x, minute_disc7_y, 40, 40);
+	}
+	if (minute() > 59) {
+		ellipse(minute_disc8_x, minute_disc8_y, 40, 40);
+	}
 
-	radius = radius * 2;
-	/*
-	let outer_disc_1_x = center_x + radius * Math.sin(0.5*x), outer_disc_1_y = center_y + radius * Math.cos(0.5*x);
-	let outer_disc_2_x = center_x - radius * Math.sin(0.5*x), outer_disc_2_y = center_y - radius * Math.cos(0.5*x);
+	// The discs for the hours
+	// The first four hour-discs are around the seconds orbit
+	// The rest are spread outwards at 3x, then 1.5x for successive 4
+	fill(180);
+	noStroke();
+	orbit_radius /= 3;
+	let number_hour_circles = hour() % 12;
 
-	ellipse(outer_disc_1_x, outer_disc_1_y, 50, 50);
-	ellipse(outer_disc_2_x, outer_disc_2_y, 50, 50);
-	*/
-	let outer_disc_3_x = center_x - radius * Math.sin(-2*x), outer_disc_3_y = center_y + radius * Math.cos(-2*x);
-	let outer_disc_4_x = center_x + radius * Math.sin(-2*x), outer_disc_4_y = center_y - radius * Math.cos(-2*x);
+	if (number_hour_circles > 4) {
+		let hour_disc1_x = - orbit_radius * Math.sin(x), hour_disc1_y = + orbit_radius * Math.cos(x);
+		let hour_disc2_x = + orbit_radius * Math.sin(x), hour_disc2_y = - orbit_radius * Math.cos(x);
+		let hour_disc3_x = - orbit_radius * Math.sin(-x), hour_disc3_y = + orbit_radius * Math.cos(-x);
+		let hour_disc4_x = + orbit_radius * Math.sin(-x), hour_disc4_y = - orbit_radius * Math.cos(-x);
 
-	ellipse(outer_disc_3_x, outer_disc_3_y, 50, 50);
-	ellipse(outer_disc_4_x, outer_disc_4_y, 50, 50);
+		ellipse(hour_disc1_x, hour_disc1_y, 50, 50);
+		ellipse(hour_disc2_x, hour_disc2_y, 50, 50);
+		ellipse(hour_disc3_x, hour_disc3_y, 50, 50);
+		ellipse(hour_disc4_x, hour_disc4_y, 50, 50);
 
-	radius = radius * 1.5;
+		number_hour_circles -= 4;
+	}
 
-	let outer_disc_7_x = center_x - radius * Math.sin(2*x), outer_disc_7_y = center_y + radius * Math.cos(2*x);
-	let outer_disc_8_x = center_x + radius * Math.sin(2*x), outer_disc_8_y = center_y - radius * Math.cos(2*x);
+	orbit_radius = orbit_radius * 3;
 
-	ellipse(outer_disc_7_x, outer_disc_7_y, 50, 50);
-	ellipse(outer_disc_8_x, outer_disc_8_y, 50, 50);
+	for (var i = 0; i < number_hour_circles; i++) {
+		let angular_position = 2 * PI * number_hour_circles / 12;
+		let planet_x = 0, planet_y = 0;
 
-	radius = radius * 1.5;
+		if (i%4 === 0) {
+			planet_x = planet_x - orbit_radius * Math.sin(x + angular_position);
+			planet_y = planet_y + orbit_radius * Math.cos(x + angular_position);
+		}
+		else if (i%4 === 1) {
+			planet_x = planet_x + orbit_radius * Math.sin(x + angular_position);
+			planet_y = planet_y - orbit_radius * Math.cos(x + angular_position);
+		}
+		else if (i%4 === 2) {
+			planet_x = planet_x - orbit_radius * Math.sin(-(x + angular_position));
+			planet_y = planet_y + orbit_radius * Math.cos(-(x + angular_position));
+		}
+		else {
+			planet_x = planet_x + orbit_radius * Math.sin(-(x + angular_position));
+			planet_y = planet_y - orbit_radius * Math.cos(-(x + angular_position));
+		}
+		ellipse(planet_x, planet_y, 50, 50);
 
-	let outer_disc_5_x = center_x - radius * Math.sin(-0.23*x), outer_disc_5_y = center_y + radius * Math.cos(-0.23*x);
-	let outer_disc_6_x = center_x + radius * Math.sin(-0.23*x), outer_disc_6_y = center_y - radius * Math.cos(-0.23*x);
-
-	ellipse(outer_disc_5_x, outer_disc_5_y, 50, 50);
-	ellipse(outer_disc_6_x, outer_disc_6_y, 50, 50);
+		if (i > 4) {
+			orbit_radius *= 1.5;
+		}
+	}
 }
-
-function render_disc(){}
